cd6eec88a57090fe99d73ac50c79ed1911a8e8e7
Byron Clark <byron@theclarkfamily.name>
2025-09-13 15:40:37 -0600

    Remove chordal holds.

--- users/manna-harbour_miryoku/byronc_config.c
+++ users/manna-harbour_miryoku/byronc_config.c
@@ -1,5 +1,4 @@
 #include QMK_KEYBOARD_H
-#include "features/achordion.h"
 
 void keyboard_post_init_user(void) {
 #if defined(RGB_MATRIX_ENABLE)
@@ -8,41 +7,4 @@ void keyboard_post_init_user(void) {
                                RGB_MATRIX_DEFAULT_SAT,
                                RGB_MATRIX_DEFAULT_VAL);
 #endif
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t* record) {
-    if (!process_achordion(keycode, record)) { return false; }
-
-    return true;
-}
-
-void matrix_scan_user(void) {
-    achordion_task();
-}
-
-bool achordion_chord(uint16_t tap_hold_keycode,
-                     keyrecord_t* tap_hold_record,
-                     uint16_t other_keycode,
-                     keyrecord_t* other_record) {
-
-    /* Consider these same hand chords as holds. This is specific to Miryoku Colemak. */
-    switch (tap_hold_keycode) {
-    case LGUI_T(KC_O):
-        if (other_keycode == KC_K) { return true; } /* macOS Slack everything key */
-        break;
-    }
-
-    return achordion_opposite_hands(tap_hold_record, other_record);
-}
-
-bool achordion_eager_mod(uint8_t mod) {
-    switch (mod) {
-    case MOD_LSFT:
-    case MOD_LCTL:
-    case MOD_LALT:
-        return true;            /* Eagerly apply ctrl, alt, and shift */
-
-    default:
-        return false;
-    }
 }
--- users/manna-harbour_miryoku/config.h
+++ users/manna-harbour_miryoku/config.h
@@ -11,7 +11,6 @@
 #undef TAPPING_TERM
 #define TAPPING_TERM 200
 
-
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
 
